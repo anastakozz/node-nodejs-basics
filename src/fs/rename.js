a2955174ea243295619f5ae02661073f30d12f87
anastakozz <117598324+anastakozz@users.noreply.github.com>
2024-04-23 17:07:32 +0300

    feat: add fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,15 @@
+import { promises as fsPromises } from "node:fs";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await fsPromises.rename(
+      "./src/fs/files/wrongFilename.txt",
+      "./src/fs/files/properFilename.md"
+    );
+  } catch (err) {
+    console.log(err);
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
