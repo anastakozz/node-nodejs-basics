c92966b95f6c64c1217888a12471dc90c89fdd31
anastakozz <117598324+anastakozz@users.noreply.github.com>
2024-04-23 13:11:41 +0300

    feat: add fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import { promises as fsPromises } from "node:fs";
+
 const create = async () => {
-    // Write your code here 
+  const content = "I am fresh and young";
+  const filePath = "./src/fs/files/fresh.txt";
+
+  try {
+    await fsPromises.writeFile(filePath, content, { flag: "wx" });
+  } catch (error) {
+    console.error(error);
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
