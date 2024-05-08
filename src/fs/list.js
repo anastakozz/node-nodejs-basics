2200c25e8a32471d5ba78affd40c56cc7908463c
anastakozz <117598324+anastakozz@users.noreply.github.com>
2024-05-08 19:06:14 +0300

    feat: add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import path from "node:path";
+import { promises as fsPromises } from "node:fs";
+
 const list = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const folderPath = path.join(__dirname, "files");
+
+  try {
+    if (!fsPromises.access(folderPath)) {
+      throw new Error("FS operation failed");
+    }
+    const files = await fsPromises.readdir(folderPath);
+    files.forEach((file) => console.log(file));
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await list();
