40f7339ca249f8e60f46667f232417d37d6eb19a
anastakozz <117598324+anastakozz@users.noreply.github.com>
2024-05-14 14:42:42 +0300

    feat: add fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,45 @@
+import { promises as fsPromises, mkdir } from "node:fs";
+import path from "node:path";
+
 const copy = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const sourcePath = path.join(__dirname, "files");
+  const destinationPath = path.join(__dirname, "files_copy");
+
+  const copyWithContents = async (source, destination) => {
+    const files = await fsPromises.readdir(source);
+
+    files.forEach(async (file) => {
+      const stat = await fsPromises.lstat(path.join(source, file));
+
+      if (stat.isDirectory()) {
+        await fsPromises.mkdir(path.join(destination, file));
+        await copyWithContents(
+          path.join(source, file),
+          path.join(destination, file)
+        );
+      }
+
+      if (stat.isFile()) {
+        await fsPromises.copyFile(
+          path.join(source, file),
+          path.join(destination, file)
+        );
+      }
+    });
+  };
+
+  try {
+    if (!fsPromises.access(sourcePath)) {
+      throw new Error("FS operation failed");
+    }
+
+    await fsPromises.mkdir(destinationPath);
+    copyWithContents(sourcePath, destinationPath);
+  } catch (error) {
+    console.error(error);
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
