ee2a81335ae193ad88fb33ebab4dfaab1cdfa954
anastakozz <117598324+anastakozz@users.noreply.github.com>
2024-05-24 15:26:46 +0300

    feat: add env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
+import { env } from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  const filteredVariables = Object.keys(env)
+    .filter((item) => item.startsWith(prefix))
+    .map((item) => `${item}=${env[item]}`)
+    .join("; ");
+  console.log(filteredVariables);
 };
 
 parseEnv();
